a0723eee18acf59814279424b8d84b19cf9d023a
yareh2 <hatena5_gir@hotmail.com>
2018-06-08 09:48:51 -0500

    readme

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,23 @@
-window.cipher = {
-  // ...
-};
+function myFunction(){
+
+let cifrar=document.getElementById("example").value;
+let offset=document.getElementById("offset").value;
+let scii=cifrar.toUpperCase();
+let scii2="";
+for(let i=0;i<scii.length;i++){
+   let posicionscii=scii.charCodeAt(i);
+   let shiftposicion=((posicionscii-65+offset)% 26 + 65);
+   let posicionscii2=String.fromCharCode(shiftposicion);
+scii2+=posicionscii2
+
+
+}
+
+//document.getElementById('palabraCifrada').value = scii;
+document.getElementById('cifrar').value=scii2;
+
+
+
+
+
+}
